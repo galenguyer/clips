361e68a20b809e234f746c035e734b1d8c399d75
Galen Guyer <galen@galenguyer.com>
2021-07-09 16:57:46 -0700

    add repl with readline

--- clips.c
+++ clips.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#include <editline/readline.h>
+#include <histedit.h>
 
 int main(int argc, char** argv) {
-  puts("Hello, world!");
-  return 0;
+    puts("clips!");
+    puts("soon to be a small lisp, written in c");
+
+    // buffer to store user input
+    while (1) {
+        char* input = readline("clips> ");
+        add_history(input);
+        free(input);
+    }
+
+    return 0;
 }
